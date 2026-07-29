7ec2e3561966922ca0e0e5555373a160c68153a9
nikoroberto90 <niko.roberto90@gmail.com>
2026-07-29 18:21:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server attivo!`);
     console.log(`- Schermo Display: http://localhost:${PORT}/display.html`);
